85f5abe4fe22bddbada22eb49295cadeb8e39809
Andy Bettisworth <andy@accreu.com>
2020-01-04 22:26:00 -0600

    Spec TODO

--- cypress/integration/sample.spec.js
+++ cypress/integration/sample.spec.js
@@ -3,7 +3,13 @@
  */
 
 describe("Sample", () => {
-  it("should have tests", () => {
-    expect(true).to.equal(true);
-  })
+  it.skip("has a set of Controls")
+  // TODO check MyLocation button exists.
+
+  it.skip("has a Map")
+  // TODO check Leaflet map rendered.
+  // TODO check default center is Austin, TX.
+  // TODO check default zoom is 11.
+  // TODO check has zoom controls available.
+  // TODO check has attribution.
 });
