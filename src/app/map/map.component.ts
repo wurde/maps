17cd35e82a710ceb4af933e83c9c21f42fecfe86
Andy Bettisworth <andy@accreu.com>
2020-01-04 23:19:16 -0600

    Install Leaflet deps

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements AfterViewInit {
+  constructor() {}
 
-  constructor() { }
+  ngAfterViewInit() {
+    console.log('init');
+    const tiles = L.tileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      }
+    );
 
-  ngOnInit() {
+    tiles.addTo(mymap);
   }
 
+  private initMap(): void {
+    this.map = L.map('map').setView([51.505, -0.09], 13);
+  }
 }
